e0f53e565680fb0d4e2e8a6b0cb81b8891a3ead5
zj <1165505801@qq.com>
2022-05-05 14:26:57 +0800

    init project

---
+++ mall-coupon/src/test/java/com/lawrance/mall/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrance.mall.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
